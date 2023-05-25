bfbbb9506227a26de4b13c40162f1d187c7d9177
juancamilobanguero <banguerojuancamilo58@gmail.com>
2023-05-24 19:42:47 -0500

    {add} create componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ButtonComponent } from './components/button/button.component';
+import { HomeComponent } from './routes/home/home.component';
+import { LoginComponent } from './routes/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ButtonComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
